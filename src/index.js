9acf217313fe6769210098c01ea7db5c78d83675
dtannyc1 <dtannyc1@gmail.com>
2023-05-08 10:35:29 -0400

    changed background url for live site, still testing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import { Game } from "./scripts/game";
 import { GameView } from "./scripts/game_view";
 
 // set background
-let body = document.querySelector("body");
-body.style.backgroundImage = "url('../assets/images/Backgrounds/asian_night.png')"
+// let body = document.querySelector("body");
+// body.style.backgroundImage = "url('../assets/images/Backgrounds/asian_night.png')"
 
-let arcadeMachine = document.querySelector("#arcade-machine");
-arcadeMachine.style.backgroundImage = "url('../assets/images/ArcadeMachineTransparentV21080p.png')"
+// let arcadeMachine = document.querySelector("#arcade-machine");
+// arcadeMachine.style.backgroundImage = "url('../assets/images/ArcadeMachineTransparentV21080p.png')"
 
 let canvas = document.querySelector("#main-canvas");
 const ctx = canvas.getContext("2d");
